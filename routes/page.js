f43fcb250f2f8ad919ac1de881e90302a9efdf11
unknown <gunwbro@gmail.com>
2020-11-03 17:33:05 +0900

    routing page

--- routes/page.js
+++ routes/page.js
@@ -2,15 +2,38 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req,res,next) => {
-    res.render('index', {
-        title: 'Jerkyb',
-    });
+router.get("/", (req, res, next) => {
+  res.render("index", {
+    title: "Jerkyb",
+    menuName: "Home",
+  });
 });
 
-router.get('/profile', (req,res,next) => {
-    res.render('profile', {
-        title: 'Jerkyb',
-    })
-})
+router.get("/profile", (req, res, next) => {
+  res.render("profile", {
+    title: "Jerkyb",
+    menuName: "Profile",
+  });
+});
+
+router.get("/project", (req, res, next) => {
+  res.render("project", {
+    title: "Jerkyb",
+    menuName: "Project",
+  });
+});
+
+router.get("/tech", (req, res, next) => {
+  res.render("tect", {
+    title: "Jerkyb",
+    menuName: "Tech",
+  });
+});
+
+router.get("/tag", (req, res, next) => {
+  res.render("tag", {
+    title: "Jerkyb",
+    menuName: "Tag",
+  });
+});
 module.exports = router;
