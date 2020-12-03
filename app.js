6ae7cae8d4541dfdf33bac075fd1db55c1c7ca76
unknown <gunwbro@gmail.com>
2020-12-03 18:53:58 +0900

    maybe

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('port', process.env.PORT || 8001);
 if (process.env.NODE_ENV === 'production') {
     app.use(morgan("combined"));
     //app.use(helmet());
-    app.use(hpp());
+    //app.use(hpp());
 } else {
     app.use(morgan('dev'));
 }
